9ee52f679c419bda529912fd20325597790dca6e
Nicolas TOUSSAINT <nicolas.toussaint@etu.emse.fr>
2022-02-03 10:09:57 +0100

    Transition to Kicad6.0 new symbol library format

--- schematic/schematic.py
+++ schematic/schematic.py
@@ -1,4 +1,4 @@
-import requests 
+import requests
 import json
 import re
 import os
@@ -9,45 +9,44 @@ from schematic.schematic_handlers import *
 
 
 template_lib_header = f"""\
-(kicad_symbol_lib (version 20210201) (generator JLC2KiCad_lib)
-"""
-
-"""
-;; created by JLC2KiCad_lib using the JLCPCB library
-;; for more info see https://github.com/TousstNicolas/JLC2KiCad_lib
+(kicad_symbol_lib (version 20210201) (generator TousstNicolas/JLC2KiCad_lib)
 """
 
 template_lib_footer = """
 )"""
-	
 
 def create_schematic(schematic_component_uuid, footprint_name, datasheet_link, library_name, output_dir):
 
 	class kicad_schematic():
 		drawing = ""
-		part = 0
 	kicad_schematic = kicad_schematic()
 
-
+	ComponentName = ""
 	for component_uuid in schematic_component_uuid:
 
-		kicad_schematic.part += 1
 
 		response = requests.get(f"https://easyeda.com/api/components/{component_uuid}")
-		if response.status_code == requests.codes.ok: 
+		if response.status_code == requests.codes.ok:
 			data = json.loads(response.content.decode())
 		else :
 			logging.error(f"create_schematic error. Requests returned with error code {response.status_code}")
 			return()
 
-		schematic_shape = data["result"]["dataStr"]["shape"]
+		schematic_shape  = data["result"]["dataStr"]["shape"]
 		symmbolic_prefix = data["result"]["packageDetail"]["dataStr"]["head"]["c_para"]["pre"].replace("?", "")
+		component_title  = data["result"]["title"].replace("/", "_").replace(" ", "_").replace(".", "_")
 
-		component_title = data["result"]["title"].replace("/", "_").replace(" ", "_")
+		if not ComponentName :
+			ComponentName = component_title
+			component_title += "_0"
+		if len(schematic_component_uuid) >= 2 and component_uuid == schematic_component_uuid[0] :
+			continue
+
 		filename = f"{output_dir}/Schematic/" + library_name + ".kicad_sym"
-		
+
 		logging.info(f"creating schematic {component_title} in {library_name}")
 
+		kicad_schematic.drawing += f'''\n    (symbol "{component_title}_0"'''
 		for line in schematic_shape :
 			args = [i for i in line.split("~") if i] # split and remove empty string in list
 			model = args[0]
@@ -56,13 +55,16 @@ def create_schematic(schematic_component_uuid, footprint_name, datasheet_link, l
 				logging.warning("Schematic : parsing model not in handler : " + model)
 			else :
 				handlers.get(model)(args[1:], kicad_schematic)
+		kicad_schematic.drawing += """\n    )"""
+
+
 
 	template_lib_component = f"""\
-  (symbol "{component_title}" (pin_names (offset 1)) (in_bom yes) (on_board yes)
+  (symbol "{ComponentName}" (pin_names (offset 1)) (in_bom yes) (on_board yes)
     (property "Reference" "{symmbolic_prefix}" (id 0) (at 0 1.27 0)
       (effects (font (size 1.27 1.27)))
     )
-    (property "Value" "{component_title}" (id 1) (at 0 -2.54 0)
+    (property "Value" "{ComponentName}" (id 1) (at 0 -2.54 0)
       (effects (font (size 1.27 1.27)))
     )
     (property "Footprint" "{footprint_name}" (id 2) (at 0 -10.16 0)
@@ -70,24 +72,21 @@ def create_schematic(schematic_component_uuid, footprint_name, datasheet_link, l
     )
     (property "Datasheet" "{datasheet_link}" (id 3) (at -2.286 0.127 0)
       (effects (font (size 1.27 1.27)) (justify left) hide)
-    )
-    (symbol "{component_title}_1_0" 
-{kicad_schematic.drawing}
-    )
-  )"""	
+    ){kicad_schematic.drawing}
+  )"""
 
 	if not os.path.exists(f"{output_dir}/Schematic"):
 		os.makedirs(f"{output_dir}/Schematic")
 
 	if os.path.exists(filename):
-		update_library(library_name, component_title, template_lib_component, output_dir)
+		update_library(library_name, ComponentName, template_lib_component, output_dir)
 	else :
-		with open(filename, "w") as f:	
+		with open(filename, "w") as f:
 			logging.info(f"writing in {filename} file")
 			f.write(template_lib_header)
 			f.write(template_lib_footer)
-		update_library(library_name, component_title, template_lib_component, output_dir)
-		
+		update_library(library_name, ComponentName, template_lib_component, output_dir)
+
 
 
 def update_library(library_name, component_title, template_lib_component, output_dir):
@@ -95,25 +94,24 @@ def update_library(library_name, component_title, template_lib_component, output
 	if component is already in library,
 	the library will be updated,
 	if not already present in library,
-	the component will be added at the beginning  
+	the component will be added at the beginning
 	"""
-	
+
 	with open(f"{output_dir}/Schematic/{library_name}.kicad_sym", 'rb+') as lib_file:
 		pattern = f'  \(symbol \"{component_title}\" \(pin_names (\n|.)*?\n  \)'
 		file_content = lib_file.read().decode()
 
 		if f'symbol "{component_title}" (pin_names' in file_content:
-			# use regex to find the old component template in the file and replace it with the new one 
+			# use regex to find the old component template in the file and replace it with the new one
 			logging.info(f'found component already in {library_name}, updating {library_name}')
 			sub = re.sub(pattern= pattern, repl= template_lib_component, string= file_content, flags = re.DOTALL, count= 1)
 			lib_file.seek(0)
-			# delete the file content and rewrite it 
+			# delete the file content and rewrite it
 			lib_file.truncate()
 			lib_file.write(sub.encode())
-		else : 
-			# move before the library footer and write the component template 
+		else :
+			# move before the library footer and write the component template
 			lib_file.seek(-len(template_lib_footer),2)
 			lib_file.truncate()
 			lib_file.write(template_lib_component.encode())
 			lib_file.write(template_lib_footer.encode())
-		
--- schematic/schematic_handlers.py
+++ schematic/schematic_handlers.py
@@ -1,235 +1,242 @@
 import logging
 
-RELATIVE_OFFSET = 10
-ABSOLUTE_OFFSET = 3000
+RELATIVE_OFFSET = 0.1
+ABSOLUTE_OFFSET_X = 40
+ABSOLUTE_OFFSET_Y = -25
 
-def h_R(data, kicad_schematic): 
+def h_R(data, kicad_schematic):
 	"""
-	S X1 Y1 X2 Y2 part dmg pen fill 
+	Rectangle handler
+	"""
 
-	Rectangle, from X1,Y1 to X2,Y2.
-	"""
-	
 	if len(data) == 12:
-		X1 = int(float(data[0])   * RELATIVE_OFFSET - ABSOLUTE_OFFSET) 	
-		Y1 = - int(float(data[1]) * RELATIVE_OFFSET - ABSOLUTE_OFFSET) 	
-		X2 = int(X1 + float(data[4]) * RELATIVE_OFFSET)						
-		Y2 = int(Y1 - float(data[5]) * RELATIVE_OFFSET)						
-	else : 
-		X1 = int(float(data[0])   * RELATIVE_OFFSET - ABSOLUTE_OFFSET) 	
-		Y1 = - int(float(data[1]) * RELATIVE_OFFSET - ABSOLUTE_OFFSET) 	
-		X2 = int(X1 + float(data[2]) * RELATIVE_OFFSET)			
-		Y2 = int(Y1 - float(data[3]) * RELATIVE_OFFSET)			
+		X1 = float(data[0])
+		Y1 = float(data[1])
+		X2 = float(X1) + float(data[4])
+		Y2 = float(Y1) + float(data[5])
+	else :
+		X1 = float(data[0])
+		Y1 = float(data[1])
+		X2 = float(X1) + float(data[2])
+		Y2 = float(Y1) + float(data[3])
 
-	part = kicad_schematic.part
-	dmg = "0"
-	pen = "0"
-	fill = "f"
+	X1 *=  RELATIVE_OFFSET
+	Y1 *= -RELATIVE_OFFSET
+	X2 *=  RELATIVE_OFFSET
+	Y2 *= -RELATIVE_OFFSET
 
-	kicad_schematic.drawing += f"\nS {X1} {Y1} {X2} {Y2} {part} {dmg} {pen} {fill}"
+	X1 -= ABSOLUTE_OFFSET_X
+	Y1 -= ABSOLUTE_OFFSET_Y
+	X2 -= ABSOLUTE_OFFSET_X
+	Y2 -= ABSOLUTE_OFFSET_Y
+
+	kicad_schematic.drawing += f"""\
+      (rectangle
+        (start {X1} {Y1})
+        (end {X2} {Y2})
+        (stroke (width 0) (type default) (color 0 0 0 0))
+        (fill (type background))
+      )"""
 
 def h_E(data, kicad_schematic):
 	"""
-	C X Y radius part dmg pen fill 
-
 	Circle
 	"""
+
 	try :
-		X1 = int(float(data[0]))*RELATIVE_OFFSET - ABSOLUTE_OFFSET
-		Y1 = int(float(data[1]))*RELATIVE_OFFSET - ABSOLUTE_OFFSET
-		radius = int(float(data[2]))
-		dmg = "0"
-		pen = "0"
-		fill = "f"
-		kicad_schematic.drawing += f"\nC {X1} {Y1} {radius} {kicad_schematic.part} {dmg} {pen} {fill}"
+		X1 = 	 float(data[0]) * RELATIVE_OFFSET - ABSOLUTE_OFFSET_X
+		Y1 = 	-float(data[1]) * RELATIVE_OFFSET - ABSOLUTE_OFFSET_Y
+		radius = float(data[2]) * RELATIVE_OFFSET
+		kicad_schematic.drawing += f"""
+      (circle
+        (center {X1} {Y1})
+        (radius {radius})
+        (stroke (width 0) (type default) (color 0 0 0 0))
+        (fill (type background))
+      )"""
 	except :
-		logging.exception("schematic Circle ")
+		logging.exception("schematic : failed to add circle")
 
 def h_P(data, kicad_schematic):
 	"""
 	Add Pin to the schematic
-
-	X name pin X Y length orientation sizenum sizename part dmg type shape	
-	
-	Pin description. The pin name is not in double quotes. When a pin has no 
-	name, parameter name is a “~”, but when a “~” is followed by a name, 
-	the name has an overbar. The pin parameter is the pin number (it need 
-	not be numeric and may be a “~”). Parameter orientation is a single letter,
-	U(p), D(own), L(eft) or R(ight). The sizenum and sizename parameters 
-	give the text sizes for the pin number and the pin name respectively. The 
-	type is a single letter: I(nput), O(utout), B(idirectional), T(ristate), 
-	P(assive), (open) C(ollector), (open) E(mitter), N(on-connected), 
-	U(nspecified), or W for power input or w of power output. If the shape is 
-	absent, the shape is a line, otherwise it is one of the letters I(nverted), 
-	C(lock), L for input-low, V for output-low (there are more shapes...). If the 
-	shape is prefixed with an “N”, the pin is invisible.
-
 	"""
-	pin_name = data[13].replace(" ", "_")
-	pin_number = data[2]
-	X =  int(data[3]) * RELATIVE_OFFSET - ABSOLUTE_OFFSET
-	Y = -int(data[4]) * RELATIVE_OFFSET + ABSOLUTE_OFFSET
-	length = 200
-	if data[5] == '0':
-		orientation = 'L'
-		X += int(length/2)
-	elif data[5] == '180':
-		orientation = 'R'
-		X -= int(length/2)
-	elif data[5] == '90':
-		orientation = 'D'
-		Y += int(length/2)
-	elif data[5] == '270':
-		orientation = 'U'
-		Y -= int(length/2)
-	else :
-		orientation = 'L'
-		logging.warning(f'Schematic : pin number {pin_number} : "{pin_name}" failed to find orientation. Using Default orientation "Left" ')
-
-	sizenum = "40"
-	sizename = "40"
-	dmg = "0"
-	shape = ""
 
 	if data[1] == '0':
-		electrical_type = "U" # Unspecified
+		electrical_type = "unspecified"
 	elif data[1] == '1':
-		electrical_type = "I" # Input
+		electrical_type = "input"
 	elif data[1] == '2':
-		electrical_type = "O" # Output
+		electrical_type = "output"
 	elif data[1] == '3':
-		electrical_type = "B" # Bidirectionnal
+		electrical_type = "bidirectional"
 	elif data[1] == '4':
-		electrical_type = "W" # Power input
-	else : 
-		electrical_type = "U" # Unspecified
+		electrical_type = "power_in"
+	else :
+		electrical_type = "unspecified"
 
-	kicad_schematic.drawing += f"\nX {pin_name} {pin_number} {X} {Y} {length} {orientation} {sizenum} {sizename} {kicad_schematic.part} {dmg} {electrical_type} {shape}"
+	pin_number = data[2]
+	pin_name = data[13]
+
+	X =  float(data[3]) * RELATIVE_OFFSET - ABSOLUTE_OFFSET_X
+	Y = -float(data[4]) * RELATIVE_OFFSET - ABSOLUTE_OFFSET_Y
+
+	if data[5] in ['0', '90', '180', '270']:
+		rotation = (int(data[5]) + 180) % 360
+	else :
+		rotation = 0
+		logging.warning(f'Schematic : pin number {pin_number} : "{pin_name}" failed to find orientation. Using Default orientation')
+
+	if rotation == 0 or rotation == 180:
+		length = abs(float(data[8].split('h')[-1])) * RELATIVE_OFFSET
+	elif rotation == 90 or rotation == 270:
+		length = abs(float(data[8].split('v')[-1])) * RELATIVE_OFFSET
+
+	# nameVisible = True if data[9].split('^^')[1] == '1' else False
+	# numberVisible = True if data[15].split('^^')[1] == '1' else False
+	# Visible3 = True if data[21].split('^^')[1] == '1' else False
+	# Visible4 = True if data[23].split('^^')[1] == '1' else False
+
+
+	kicad_schematic.drawing += f"""
+      (pin {electrical_type} line
+        (at {X} {Y} {rotation})
+        (length {length})
+        (name "{pin_name}" (effects (font (size 0.612 0.612))))
+        (number "{pin_number}" (effects (font (size 0.612 0.612))))
+      )"""
 
 def h_T(data, kicad_schematic):
 	"""
-	T angle X Y size hidden part dmg text italic bold Halign Valign
-
-	Text (which is not in a field). Parameter angle is in 0.1 degrees. Parameter
-	hidden is 0 for visible text and 1 for hidden text. The text can be in double
-	quotes, or it can be unquoted, but with the ~ character replacing spaces. 
-	Parameter italic is the word “Italic” for italic text, or “Normal” for upright
-	text. Parameter bold is 1 for bold and 0 for normal. Parameters Halign 
-	and Valign are for the text alignment: C(entred), L(eft), R(ight), T(op) or 
-	B(ottom).
+	Annotation handler
 	"""
-	try :
-		angle = int(data[3])*10 
-		X = int(float(data[1]))*RELATIVE_OFFSET - ABSOLUTE_OFFSET
-		Y = int(float(data[2]))*RELATIVE_OFFSET - ABSOLUTE_OFFSET
-		if data[10] == "comment" or data[5] == "comment":
-			size = 80
-		else :
-			size = int(data[5].replace('pt', ''))*10
-		hidden = "0"
-		part = kicad_schematic.part
-		dmg = "0"
-		text = data[6].replace(' ', '~')
-		italic = "Normal"
-		bold = "0"
-		Halign = "C"
-		Valign = "C"
 
-		kicad_schematic.drawing += f"\nT {angle} {X} {Y} {size} {hidden} {part} {dmg} {text}  {italic} {bold} {Halign} {Valign}" 
+	try :
+		logging.warning("Annotation handler not yet implemented")
+		X = float(data[1]) * RELATIVE_OFFSET - ABSOLUTE_OFFSET_X
+		Y = float(data[2]) * RELATIVE_OFFSET - ABSOLUTE_OFFSET_Y
+		angle = float(data[3])*10
+
+		text = data[6]
+
+		fontSize    = data[6]
+		fontWeight = data[7]
+
+	# 	if data[10] == "comment" or data[5] == "comment":
+	# 		size = 80
+	# 	else :
+	# 		size = float(data[5].replace('pt', ''))*10
+
+
+	# 	kicad_schematic.drawing += f"""
+    #   (text
+    #     "{text}"
+    #     (at {X} {Y} {angle})
+    #     (effects
+    #       (font
+    #         (size {fontSize} {fontWeight})
+    #         [(thickness THICKNESS)]
+    #         [bold]
+    #         [italic]
+    #       )
+    #       [(justify [left | right] [top | bottom] [mirror])]
+    #       [hide]
+    #     )
+    #   )"""
 	except :
 		logging.exception("failed to add text to schematic")
-		
+
 def h_PL(data, kicad_schematic):
 	"""
-	P count part dmg pen X Y 
-
-	fill Polygon with count vertices, and an X,Y position for each vertex. A filled 
-	polygon is implicitly closed, other polygons are open.
+	Polygone handler
 	"""
 
 	try :
-		count = int(len(data[0].split(" "))/2)
-		dmg = 0
-		pen = 0
-		fill = "f"
-		kicad_schematic.drawing += f"\nP {count} {kicad_schematic.part} {dmg} {pen} {' '.join([str(round(float(i)*RELATIVE_OFFSET - ABSOLUTE_OFFSET)) for i in data[0].split(' ')])} {fill}"
+		pathString = data[0].split(' ')
+		polypts = []
+		for i, _ in enumerate(pathString[::2]):
+			polypts.append(f"(xy {float(pathString[2*i]) * RELATIVE_OFFSET - ABSOLUTE_OFFSET_X} {-float(pathString[2*i+1]) * RELATIVE_OFFSET - ABSOLUTE_OFFSET_Y})")
+		polystr = '\n          '.join(polypts)
+
+		kicad_schematic.drawing += f"""
+      (polyline
+        (pts
+          {polystr}
+        )
+        (stroke (width 0) (type default) (color 0 0 0 0))
+        (fill (type none))
+      )"""
 	except :
 		logging.exception("Schematic : failed to add a polygone")
 
 def h_PG(data, kicad_schematic):
 	"""
-	closed polygone handler 
+	Closed polygone handler
 	"""
-	
+
 	try :
-		count = int(len(data[0].split(" "))/2)
-		dmg = 0
-		pen = 0
-		fill = 'f'
-		kicad_schematic.drawing += f"\nP {count + 1} {kicad_schematic.part} {dmg} {pen} {' '.join([str(round(float(i)*RELATIVE_OFFSET - ABSOLUTE_OFFSET)) for i in data[0].split(' ') + data[0].split(' ')[:2]])} {fill}"
+		pathString = data[0].split(' ')
+		polypts = []
+		for i, _ in enumerate(pathString[::2]):
+			polypts.append(f"(xy {float(pathString[2*i]) * RELATIVE_OFFSET - ABSOLUTE_OFFSET_X} {-float(pathString[2*i+1]) * RELATIVE_OFFSET - ABSOLUTE_OFFSET_Y})")
+		polypts.append(polypts[0])
+		polystr = '\n          '.join(polypts)
+
+		kicad_schematic.drawing += f"""
+      (polyline
+        (pts
+          {polystr}
+        )
+        (stroke (width 0) (type default) (color 0 0 0 0))
+        (fill (type background))
+      )"""
 	except :
 		logging.exception("Schematic : failed to add a polygone")
 
 def h_PT(data, kicad_schematic):
 	"""
-	triangle handler
-	
-	P count part dmg pen X Y 
-
-	fill Polygon with count vertices, and an X,Y position for each vertex. A filled 
-	polygon is implicitly closed, other polygons are open.
+	Triangle handler
 	"""
 
-	try :
-		# Points = ' '.join([str(round(float(i)*RELATIVE_OFFSET - ABSOLUTE_OFFSET)) for i in data[0].split(' ') + data[0].split(' ')[:2]])
-		vertex = [str(round(float(i)*RELATIVE_OFFSET - ABSOLUTE_OFFSET)) for i in data[0].split(' ') if i not in ['M', 'L', 'Z', '']]
-		vertex += vertex[:2]
-		count = 3
-		dmg = 0
-		pen = 0
-		fill = 'f'
-		kicad_schematic.drawing += f"\nP {count + 1} {kicad_schematic.part} {dmg} {pen} {' '.join(vertex)} {fill}"
-	except :
-		logging.exception("Schematic : failed to add a polygone")
-
+	data[0] = data[0].replace('M ', '').replace('L ', '').replace(' Z ', '')
+	h_PG(data, kicad_schematic)
 
 def h_A(data, kicad_schematic):
 	"""
-	A X Y radius start end part dmg pen fill Xstart Ystart Xend Yend
-
-	Arc. The start and end parameters are angles in 0.1 degrees. The Xstart
-	and Ystart parameters give the coordinate of the start point; it can be
-	calculated from the radius and the start angle. Similarly, the Xend and
-	Yend parameters give the coordinate of the end point. The arc is drawn in
-	counter-clockwise direction, but the angles are swapped if there
-	(normalized) difference exceeds 180 degrees
+	Arc handler
 	"""
 
-	pos = data[0].split(" ")
-	Xstart = int(float(pos[1])) * RELATIVE_OFFSET * 1.2 - 2 * ABSOLUTE_OFFSET
-	Ystart = int(float(pos[2])) * RELATIVE_OFFSET * 1.2 + ABSOLUTE_OFFSET 
-	Xend = int(float(pos[9]))   * RELATIVE_OFFSET * 1.2 - 2 * ABSOLUTE_OFFSET
-	Yend = int(float(pos[10]))  * RELATIVE_OFFSET * 1.2 + ABSOLUTE_OFFSET
-	X = int((Xstart + Xend)/4) 
-	Y = int((Ystart + Yend)/2) 
+	pathString = data[0].split(" ")
+	Xstart 	= float(pathString[1])  * RELATIVE_OFFSET - ABSOLUTE_OFFSET_X
+	Ystart 	= float(pathString[2])  * RELATIVE_OFFSET - ABSOLUTE_OFFSET_Y
+	radius  = float(pathString[4])  * RELATIVE_OFFSET
+	Xend 	= float(pathString[9])  * RELATIVE_OFFSET - ABSOLUTE_OFFSET_X
+	Yend 	= float(pathString[10]) * RELATIVE_OFFSET - ABSOLUTE_OFFSET_Y
 
-	radius = int((float(pos[4])/2) * RELATIVE_OFFSET * 1.2)
-	start = "1800" 
-	end = "0" 
-	part = kicad_schematic.part 
-	dmg = 0
-	pen = 0
+	Xmid = (Xstart + Xend) / 2
+	Ymid = (Ystart + Yend) / 2 + radius
 
-	kicad_schematic.drawing += f"\nA {X} {Y} {radius} {start} {end} {part} {dmg} {pen}"
-
+	kicad_schematic.drawing += f"""
+      (arc
+        (start {Xstart} {Ystart})
+        (mid {Xmid} {Ymid})
+        (end {Xend} {Yend})
+        (stroke (width 0) (type default) (color 0 0 0 0))
+        (fill (type none))
+      )"""
 
 handlers = {
-	"R" : h_R, 
-	"E" : h_E, 
-	"P" : h_P, 
+	"R" : h_R,
+	"E" : h_E,
+	"P" : h_P,
 	"T" : h_T,
 	"PL" : h_PL,
 	"PG" : h_PG,
 	"PT" : h_PT,
 	"A" : h_A,
+	# "J" : h_NotYetImplemented,
+	# "N" : h_NotYetImplemented,
+	# "BE" : h_NotYetImplemented,
+	# "AR" : h_NotYetImplemented,
+	# "O" : h_NotYetImplemented,
 	}
