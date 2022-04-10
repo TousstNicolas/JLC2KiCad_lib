70250fa4b70864c1f2fdc61c06abce8b436713ab
Nicolas TOUSSAINT <68825213+TousstNicolas@users.noreply.github.com>
2022-04-10 18:09:29 +0200

    update version to 1.0.3 for #9

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
 	long_description=long_description,
 	long_description_content_type="text/markdown",
 	url="https://github.com/TousstNicolas/JLC2KiCad_lib",
-	version="1.0.2",
+	version="1.0.3",
 	author="TousstNicolas",
 	license="MIT",
 	install_requires=["KicadModTree",],
